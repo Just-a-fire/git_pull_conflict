f43aa2ab32d96c5aabfb802b12bb599ba49be911
Albert Maximov <albert.maksimov@cheb.wikimart.ru>
2017-01-27 11:24:33 +0300

    first commit

---
+++ public_html/index.php
@@ -0,0 +1,6 @@
+<?php 
+for ($i=0; $i < 10; $i++) { 
+	echo $i . '<br>';
+}
+ ?>
+<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut obcaecati blanditiis asperiores vitae, expedita dolorem aperiam totam ab labore deserunt?</h3>
